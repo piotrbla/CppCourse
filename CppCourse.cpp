d372ded4d8c987038101c43639054d0752f6884f
PiotrB <piotrb@gmail.com>
2023-03-19 16:18:33 +0100

    Add project files.

---
+++ CppCourse.cpp
@@ -0,0 +1,17 @@
+/* CppCourse.cpp : This file contains the 'main' function.Program execution begins and ends there. */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::cout << "Your name:" << std::endl;
+    std::string name;
+	std::cin >> name;
+    
+    std::cout << "Hello, " << name << "!\n";
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
