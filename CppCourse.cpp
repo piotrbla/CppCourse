eba5c4a246eea03952a52b5a5b38834b7aa5bd0c
PiotrB <piotrb@gmail.com>
2023-03-19 17:58:28 +0100

    Greeting improved

--- CppCourse.cpp
+++ CppCourse.cpp
@@ -5,13 +5,23 @@
 
 int main()
 {
-	std::string greeting = "Hello World!";
-	std::cout << "Your name:" << std::endl;
+    std::cout << "Your name:" << std::endl;
     std::string name;
-	std::cin >> name;
+    std::cin >> name;
     
-    std::cout << "Hello, " << name << "!\n";
-    return 0;
+	const std::string greeting = "Hello, " + name + "!";
+	const std::string spaces(greeting.size(), ' ');
+	const std::string second = "* " + spaces + " *";
+	const std::string first(second.size(), '*');
+	
+	std::cout << std::endl;
+	std::cout << first << std::endl;
+	std::cout << second << std::endl;
+	std::cout << "* " << greeting << " *" << std::endl;
+	std::cout << second << std::endl;
+	std::cout << first << std::endl;
+	
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
