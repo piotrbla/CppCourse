6d150a71f7a8e005195752209c6239b8f4ad0664
PiotrB <piotrb@gmail.com>
2023-06-29 14:22:01 +0200

    E16 - all analysis

--- tests.cpp
+++ tests.cpp
@@ -6,6 +6,7 @@
 #include <list>			//list
 #include <algorithm>	//sort
 #include <chrono>		//chrono	
+#include <numeric>		//accumulate
 #include <fstream>
 #include "median.h"
 #include "grade.h"
@@ -380,6 +381,40 @@ void write_analysis(std::ostream& out, const string& name,
 		<< endl;
 }
 
+double average(const vector<double>& v)
+{
+	return std::accumulate(v.begin(), v.end(), 0.0) / v.size();
+}
+
+double average_grade(const Student_info& s)
+{
+	return grade(s.midterm, s.final, average(s.homework));
+}
+
+double average_analysis(const vector<Student_info>& students)
+{
+	vector<double> grades;
+	transform(students.begin(), students.end(), back_inserter(grades), average_grade);
+	return median(grades);
+}
+
+double optimistic_median(const Student_info& s)
+{
+	vector<double> nonzero;
+	remove_copy(s.homework.begin(), s.homework.end(), back_inserter(nonzero), 0);
+	if (nonzero.empty())
+		return grade(s.midterm, s.final, 0.0);
+	else
+		return grade(s.midterm, s.final, median(nonzero));
+}
+
+double optimistic_median_analysis(const vector<Student_info>& students)
+{
+	vector<double> grades;
+	transform(students.begin(), students.end(), back_inserter(grades), optimistic_median);
+	return median(grades);
+}
+
 void analysis_test()
 {
 	vector<Student_info> students_vector = read_students();
@@ -403,6 +438,8 @@ void analysis_test()
 		return;
 	}
 	write_analysis(cout, "median", median_analysis, did, didnt);
+	write_analysis(cout, "average", average_analysis, did, didnt);
+	write_analysis(cout, "median of homework turned in", optimistic_median_analysis, did, didnt);
 
 }
 
@@ -419,7 +456,7 @@ void tests()
 	//find_fail_test_list_5();
 	//find_did_all_hw_test();
 	analysis_test();
-
+	
 	//one_person_grade_tests();
 	//read_hw_test();
 }
