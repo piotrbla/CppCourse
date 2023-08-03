fa15082beb521ea4a0b813aabf26aa9d65c0e5a2
PiotrB <piotrb@gmail.com>
2023-08-03 13:51:24 +0200

    Vector tests cleanup

--- VectorTestcpp.cpp
+++ VectorTestcpp.cpp
@@ -36,39 +36,3 @@ int vectorTester()
 
 	return 0;
 }
-
-
-
-
-
-
-//#include <numeric>		//accumulate
-//using std::accumulate;
-//double sum = accumulate(
-//	numbers.begin(),
-//	numbers.end(),
-//	0.0
-//);
-//  sum = std::accumulate(numbers.begin(), numbers.end(),
-//decltype(numbers)::value_type(0));
-//auto result = std::reduce(numbers.begin(), numbers.end());
-//cout << "The sum of the numbers is "
-//<< sum << endl;
-//cout << "The average of the numbers is "
-//<< sum / size << endl;
-//std::vector<int> sums(data.size());
-//
-////SUM ALL NUMBERS IN A GIVEN VECTOR.
-//inclusive_scan(data.begin(), data.end(),
-//	sums.begin());
-//
-////SUM OF FIRST 5 ELEMENTS.
-//std::cout << "Sum of first 5 elements :: " << sums[4] << std::endl;
-//
-////SUM OF ALL ELEMENTS
-//std::cout << "Sum of all elements :: " << sums[data.size() - 1] << std::endl;
-//WITHOUT EXECUTION POLICY
-//int sum = std::reduce(vec.begin(), vec.end());
-//
-////TAKING THE ADVANTAGE OF EXECUTION POLICIES
-//int sum2 = std::reduce(std::execution::par, vec.begin(), vec.end());
