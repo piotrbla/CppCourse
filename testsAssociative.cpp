f8e800e38c40524b67a108029eef6526f52a1571
PiotrB <piotrb@gmail.com>
2023-08-03 14:02:47 +0200

    Tests fo associative arrays added

---
+++ testsAssociative.cpp
@@ -0,0 +1,39 @@
+#include "testsAssociative.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <stdexcept>
+#include <iomanip>
+#include <ios>
+#include <numeric>
+#include <iterator>
+#include <list>
+#include <cctype>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::map;
+
+void tests_map()
+{
+	
+}
+
+void tests_associative()
+{
+	tests_map();
+	//tests_multimap();
+	//tests_set();
+	//tests_multiset();
+	//tests_unordered_map();
+	//tests_unordered_multimap();
+	//tests_unordered_set();
+	//tests_unordered_multiset();
+}
---
+++ testsAssociative.h
@@ -0,0 +1,5 @@
+#ifndef _TESTS_ASSOCIATIVE_H
+#define _TESTS_ASSOCIATIVE_H
+void tests_associative();
+#endif //_TESTS_ASSOCIATIVE_H
+
