d3899238cd7528009d0d9ea7bf6b0b987651b2e4
PiotrB <piotrb@gmail.com>
2023-08-14 18:41:39 +0200

    Function template - E19 - median

--- testsTemplateFunction.cpp
+++ testsTemplateFunction.cpp
@@ -1,6 +1,7 @@
 #include "testsTemplateFunction.h"
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -54,14 +55,35 @@ void test_template_function_new_cpp()
 {
 	cout << demo_abs_template<double>(6.1) << endl;
 	cout << demo_abs_template<double>(-3.1) << endl;
-	cout << demo_abs_template<int>(5.1) << endl;
+	cout << demo_abs_template<int>(5) << endl;
 	cout << demo_abs_template<int>(-3) << endl;
 	cout << endl;
 }
 
+template <class T>
+T median(vector<T> vec)
+{
+	typedef typename vector<T>::size_type vec_size;
+	vec_size size = vec.size();
+	if (size == 0)
+		throw std::domain_error("median of an empty vector");
+	std::sort(vec.begin(), vec.end());
+	vec_size mid = size / 2;
+	return size % 2 == 0 ? (vec[mid] + vec[mid - 1]) / 2 : vec[mid];
+}
+
+void test_template_median()
+{
+	vector<int> v1 = { 1, 2, 3, 4, 5 };
+	vector<double> v2 = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+	cout << median(v1) << endl;
+	cout << median(v2) << endl;
+}
+
 void tests_template_function()
 {
-	test_template_function_old_c();
-	test_template_function_old_cpp();
-	test_template_function_new_cpp();
+	//test_template_function_old_c();
+	//test_template_function_old_cpp();
+	//test_template_function_new_cpp();
+	//test_template_median();
 }
